3174e2ddd8e541dd392477456c9b681ef00b05b1
Celestarien <julien.giovannangeli@ynov.com>
2021-10-20 16:57:08 +0200

    SocialHunt main file

---
+++ SocialHunt.py
@@ -0,0 +1,6 @@
+print("  ___                ___  ")
+print(" (o o)              (o o) ")
+print("(  V  ) SocialHunt (  V  )")
+print("--m-m----------------m-m--")
+
+action = input('Que veux-tu faire ?\n')
