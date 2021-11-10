3c726df9399cbe027e11b944103930ee812321cf
Celestarien <celestarien@gmail.com>
2021-11-10 10:52:50 +0100

    working with Protonmail

--- SocialHunt.py
+++ SocialHunt.py
@@ -1,6 +1,11 @@
+from modules import Search_by_email
+
+
 print("  ___                ___  ")
 print(" (o o)              (o o) ")
 print("(  V  ) SocialHunt (  V  )")
 print("--m-m----------------m-m--")
 
+
+Search_by_email.main()
 action = input('Que veux-tu faire ?\n')
--- modules/Search_by_email.py
+++ modules/Search_by_email.py
@@ -1,12 +1,43 @@
 import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import ssl
 
 # Packages pip install googlesearch-python
 
-from googlesearch import search
+# from googlesearch import search
 
 
 def main():
-    emailSearched = input('Quelle email veux-tu tracker ?\n')
-    request = requests.get('https://twitter.com/' + emailSearched)
-    search(emailSearched, num_results=1)
-    x = input('')
+    emailSearched = input('Quelle est la personne dont tu veux tracker l\'email ?\n')
+
+    # Protonmail
+
+    print('###################### PROTONMAIL ######################')
+    req = urllib.request.Request('https://api.protonmail.ch/pks/lookup?op=index&search=' + emailSearched + '@protonmail.com')
+    gcontext = ssl.SSLContext()
+    with urllib.request.urlopen(req, context=gcontext) as response:
+        page = response.read()
+        soup = BeautifulSoup(page, features = "lxml")
+        for line in soup:
+            print(line.text)
+        print('#####################################################')
+
+
+
+    # Yahoo
+    print('###################### Yahoo ######################')
+    url = 'http://login.yahoo.com/?.src=ym&pspid=2023392333&activity=ybar-mail&.lang=fr-FR&.intl=fr&.done=https%3A%2F%2Fmail.yahoo.com%2Fd%3F.intl%3Dfr%26.lang%3Dfr-FR%26pspid%3D2023392333%26activity%3Dybar-mail&username=' + emailSearched + '@yahoo.com'
+    requests.Session()
+    req = requests.post(url=url)
+    f = open('test.txt', 'w+')
+    f.write(str(req.text))
+    find = False
+    for line in req.text:
+        if 'error-msg hide' in str(line):
+            find = True
+    if find == True:
+        print( emailSearched + '@yahoo.com')
+    else:
+        print('Pas d\'email trouvé à ce nom')
+    print('#####################################################')
